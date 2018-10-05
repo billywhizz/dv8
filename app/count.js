2e160aa5f4d6bbdc708dde5e3bebb21347f62def
Andrew Johnston <apjohnsto@gmail.com>
2018-10-05 06:07:11 +0100

    minimal tty example with perf

--- app/count.js
+++ app/count.js
@@ -1,38 +1,13 @@
-const start = Date.now()
-require('./base.js')
-let mem = memoryUsage()
-function toMiB(bytes) {
-    return Math.floor((bytes / (1024 * 1024))*100) / 100
-}
-function toMib(bytes) {
-    return Math.floor(((bytes * 8) / (1024 * 1024))*100) / 100
-}
-const ANSI_RED='\u001b[31m'
-const ANSI_MAGENTA='\u001b[35m'
-const ANSI_DEFAULT='\u001b[0m'
-const ANSI_CYAN='\u001b[36m'
-const ANSI_GREEN='\u001b[32m'
-function printStats(pipe, end = Date.now()) {
-    const { bytes, name } = pipe
-    const elapsed = end - start
-    const seconds = elapsed / 1000
-    const MiBRate = Math.floor(toMiB(bytes) / seconds * 100) / 100
-    const MibRate = Math.floor(toMib(bytes) / seconds * 100) / 100
-    print(`${ANSI_MAGENTA}${name.padEnd(12, ' ')}${ANSI_DEFAULT}: ${bytes.toString().padStart(12, ' ')} ${ANSI_CYAN}bytes${ANSI_DEFAULT} ${seconds.toString().padStart(7, ' ')} ${ANSI_CYAN}sec${ANSI_DEFAULT} ${MibRate.toString().padStart(10, ' ')} ${ANSI_CYAN}Mib/s${ANSI_DEFAULT} ${MiBRate.toString().padStart(10, ' ')} ${ANSI_CYAN}MiB/s${ANSI_DEFAULT} ${toMiB(mem.rss.toString().padStart(10, ' '))} ${ANSI_GREEN}RSS${ANSI_DEFAULT} ${toMiB(mem.heapUsed.toString().padStart(10, ' '))} ${ANSI_GREEN}Heap${ANSI_DEFAULT} ${toMiB(mem.external.toString().padStart(10, ' '))} ${ANSI_GREEN}Ext${ANSI_DEFAULT}`)
-}
-const timer = setInterval(() => {
-    mem = memoryUsage()
-    //printStats(stdin, Date.now())
-}, 1000)
+const { start, stop } = require('./meter.js')
 const { TTY } = module('tty', {})
 const b = new Buffer()
 b.alloc(64 * 1024)
 const stdin = new TTY(0, len => stdin.bytes += len, () => {
-    printStats(stdin, Date.now())
     stdin.close()
-    timer.stop()
+    stop(stdin)
 }, () => {})
 stdin.setup(b)
 stdin.bytes = 0
 stdin.name = 'count.stdin'
+stdin.timer = start(stdin)
 stdin.resume()
--- app/pipe.js
+++ app/pipe.js
@@ -1,122 +1,27 @@
-const { TTY } = module('tty', {})
-const start = Date.now()
-const BUFFER_SIZE = 64 * 1024
+const { start, stop } = require('./meter.js')
+const tty = module('tty', {})
+const { TTY } = tty
 const b = new Buffer()
-b.alloc(BUFFER_SIZE)
-
-function toMib(bytes) {
-    return bytes * 8n / (1024n * 1024n)
-}
-
-function stdinStats() {
-    const st = new BigUint64Array(7)
-    stdin.stats(st)
-    return {
-        read: st[0],
-        error: st[1],
-        pause: st[2],
-        close: st[3],
-        data: st[4],
-        resume: st[5],
-        end: st[6],
-    }
-}
-
-function dumpStdin() {
-    const stats = stdinStats()
-print(`
--------------------------------
-stdin
--------------------------------
-read:        ${stats.read}
-error:       ${stats.error}
-pause:       ${stats.pause}
-close:       ${stats.close}
-data:        ${stats.data}
-resume:      ${stats.resume}
-end:         ${stats.end}
--------------------------------
-`)
-}
-
-function stdoutStats() {
-    const st = new BigUint64Array(10)
-    stdout.stats(st)
-    return {
-        written: st[0],
-        incomplete: st[1],
-        full: st[2],
-        error: st[3],
-        drain: st[4],
-        close: st[5],
-        maxQueue: st[6],
-        alloc: st[7],
-        free: st[8],
-        eagain: st[9]
-    }
-}
-
-function dumpStdout() {
-    const stats = stdoutStats()
-print(`
--------------------------------
-stdout
--------------------------------
-written:     ${stats.written}
-incomplete:  ${stats.incomplete}
-full:        ${stats.full}
-error:       ${stats.error}
-drain:       ${stats.drain}
-close:       ${stats.close}
-maxQueue:    ${stats.maxQueue}
-alloc:       ${stats.alloc}
-free:        ${stats.free}
-eagain:      ${stats.eagain}
--------------------------------
-`)
-}
-
-function dumpMetrics(metrics) {
-    print(`${new Date()} r: ${toMib(metrics.readRate)} Mib w: ${toMib(metrics.writeRate)} Mib`)
-}
-
-function getMetrics() {
-    const finish = Date.now()
-    const elapsed = finish - start
-    const elapsedSeconds = BigInt(Math.ceil(elapsed / 1000))
-    const stdin = stdinStats()
-    const stdout = stdoutStats()
-    const readRate = stdin.read / elapsedSeconds
-    const writeRate = stdout.written / elapsedSeconds
-    return { start, finish, stdin, stdout, elapsed, elapsedSeconds, readRate, writeRate }
-}
-
-function verify() {
-    const { start, finish, stdin, stdout, elapsed, elapsedSeconds, readRate, writeRate } = getMetrics()
-    print(`no errors      : ${stdin.error === 0n && stdout.error === 0n}`)
-    print(`read = written : ${stdin.read === stdout.written}`)
-    print(`allocations    : ${stdout.alloc === stdout.free}`)
-    print(`allocations 2  : ${stdout.alloc === (stdout.eagain + stdout.incomplete)}`)
-    print(`writes = reads : ${stdin.data === (stdout.full + stdout.incomplete + stdout.eagain)}`)
-    print(`stdin.end      : ${stdin.end === 1n}`)
-    print(`stdin.close    : ${stdin.close === 1n}`)
-    print(`stdout.close   : ${stdout.close === 1n}`)
-    print(`pause = resume : ${stdin.pause === stdin.resume}`)
-}
-
+b.alloc(64 * 1024)
 const stdin = new TTY(0, len => {
+    stdin.bytes += len
     const r = stdout.write(len)
     if (r < 0) return stdout.close()
+    stdout.bytes += len // assume they will go
     if (r < len && stdout.queueSize() >= (64 * 1024)) stdin.pause()
-}, () => stdin.close(), () => stdout.close())
+}, () => {
+    stop(stdin)
+    stdin.close()
+}, () => stdout.close())
 const stdout = new TTY(1, () => {
-    timer.stop()
-    const metrics = getMetrics()
-    dumpMetrics(metrics)
-    verify()
+    stop(stdout)
 }, () => stdin.resume(), e => { print(`write error: ${e}`) })
 stdin.setup(b)
 stdout.setup(b)
+stdin.bytes = 0
+stdout.bytes = 0
+stdin.name = 'pipe.stdin'
+stdout.name = 'pipe.stdout'
+stdin.timer = start(stdin)
+stdout.timer = start(stdout)
 stdin.resume()
-const timer = new Timer()
-timer.start(() => dumpMetrics(getMetrics()), 1000, 1000)
