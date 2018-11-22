cb3adc66e08edb67e8d86555aadcd2e10eaaa305
Andrew Johnston <apjohnsto@gmail.com>
2018-11-22 22:14:00 +0000

    cleanup

--- examples/platform/worker.js
+++
@@ -1,8 +0,0 @@
-require('./lib/ipc.js')
-process.onMessage(message => {
-  console.log(JSON.stringify(message))
-  const tt = setTimeout(() => {
-    const { env, args } = process
-    process.send({ env, args, PID, TID })
-  }, 1000)
-})
--- examples/platform2/worker.js
+++
@@ -1,9 +0,0 @@
-require('./lib/ipc.js')
-
-process.onMessage(message => {
-  console.log(JSON.stringify(message))
-  setTimeout(() => {
-    const { env, args } = process
-    process.send({ env, args, PID, TID })
-  }, 1000)
-})
